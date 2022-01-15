7f1f5f8434ef6207b38dd00da99a60c0356153c4
Zuka Chilachava <zchil19@freeuni.edu.ge>
2022-01-15 18:36:06 +0400

    Update wallet_repository.py

--- app/infra/in_memory/wallet_repository.py
+++ app/infra/in_memory/wallet_repository.py
@@ -18,13 +18,13 @@ class InMemoryWalletRepository:
             self.number_of_wallets[user_id] += 1
 
     def get_wallet_amount(self, user_id: int) -> int:
-        return self.number_of_wallets.get(user_id)
+        return self.number_of_wallets.get(user_id, -1)
 
     def get_balance(self, wallet_address: str) -> float:
-        return self.wallets.get(wallet_address)
+        return self.wallets.get(wallet_address, -1)
 
     def is_my_wallet(self, user_id: int, wallet_address: str) -> bool:
-        wallet_list: list[str] = self.wallets_for_user.get(user_id)
+        wallet_list: list[str] = self.wallets_for_user.get(user_id, [])
 
         for address in wallet_list:
             if address == wallet_address:
