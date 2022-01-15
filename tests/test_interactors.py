4fb55fe1f3c6526a315ea3e9bbc8d2ec6d078cd2
Gnems19 <71854341+Gnems19@users.noreply.github.com>
2022-01-15 19:53:33 +0400

    adding tests for interactors

---
+++ tests/test_interactors.py
@@ -0,0 +1,30 @@
+import pytest
+
+from app.core.users.users_interactor import IUserRepository, UsersInteractor
+from app.infra.in_memory.user_in_memory import UserInMemoryRepository
+from app.infra.in_memory.wallet_repository import InMemoryWalletRepository
+
+
+@pytest.fixture()
+def users_interactor() -> UsersInteractor:
+    return UsersInteractor(user_repo=UserInMemoryRepository())
+
+@pytest.fixture()
+def wallet_interactor() -> UsersInteractor:
+    return UsersInteractor(user_repo=InMemoryWalletRepository())
+
+
+def test_users_interactor_generate_new_api_key(users_interactor: UsersInteractor):
+    first_user = users_interactor.generate_new_api_key()
+    second_user = users_interactor.generate_new_api_key()
+    assert first_user != second_user
+
+
+def test_users_interactor_generate_new_api_key_multiple_calls(users_interactor: UsersInteractor):
+    for i in range(1000):
+        first_user = users_interactor.generate_new_api_key()
+        second_user = users_interactor.generate_new_api_key()
+        assert first_user != second_user
+
+
+
