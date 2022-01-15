20a301cfe7ab1aa81958a8332b0717db604d9584
Andria Kilasonia <akila19@freeuni.edu.ge>
2022-01-15 16:15:44 +0400

    modified setup

--- app/runner/setup.py
+++ app/runner/setup.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 
 from app.core.facade import WalletService
 from app.infra.fastapi.api import wallet_api
+from app.infra.in_memory.user_in_memory import UserInMemoryRepository
 
 
 def setup() -> FastAPI:
     app = FastAPI()
 
     app.include_router(wallet_api)
-    app.state.core = WalletService()
+    app.state.core = WalletService.create(
+        UserInMemoryRepository()
+    )
 
     return app
