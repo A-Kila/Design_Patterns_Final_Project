fff8f15676ccee98c2a3f22f5b24b65a20485cec
Zuka Chilachava <zchil19@freeuni.edu.ge>
2022-01-15 19:21:46 +0400

    Update wallets_interactor.py

--- app/core/wallets/wallets_interactor.py
+++ app/core/wallets/wallets_interactor.py
@@ -6,8 +6,15 @@ class IRateApi(Protocol):
         pass
 
 
-class IWalletRepository:
-    pass
+class IWalletRepository(Protocol):
+    def create_wallet(self, user_id: int, wallet_address: str, balance: float) -> None:
+        pass
+
+    def get_wallet_amount(self, user_id: int) -> int:
+        pass
+
+    def get_balance(self, wallet_address: str) -> float:
+        pass
 
 
 class WalletsInteractor:
