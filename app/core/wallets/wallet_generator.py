6fd9494e227db9f4454071191242a86bcdd12fb8
rati3 <rbreg18@freeuni.edu.ge>
2022-01-15 19:57:58 +0400

    Add wallet request, response dataclasses

--- app/core/wallets/wallet_generator.py
+++ app/core/wallets/wallet_generator.py
@@ -19,3 +19,15 @@ class WalletGetResponse:
     balance_btc: float
     balance_usd: float
     wallet_address: str
+
+
+@dataclass
+class WalletPostRequest:
+    api_key: str
+
+
+@dataclass
+class WalletPostResponse:
+    balance_btc: float
+    balance_usd: float
+    wallet_address: str
