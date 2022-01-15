42bfe9023957d18db68adb7da117580e1883fa75
Zuka Chilachava <zchil19@freeuni.edu.ge>
2022-01-15 19:14:01 +0400

    response request

--- app/core/wallets/wallet_generator.py
+++ app/core/wallets/wallet_generator.py
@@ -6,3 +6,16 @@ class Wallet:
     user_id: int
     wallet_address: str
     balance: float
+
+
+@dataclass
+class WalletGetRequest:
+    api_key: str
+    wallet_address: str
+
+
+@dataclass
+class WalletGetResponse:
+    balance_btc: float
+    balance_usd: float
+    wallet_address: str
