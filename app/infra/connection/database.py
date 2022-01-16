abd2ffec1fa703e1b778f0857ef23b2abc9d9d68
Zuka Chilachava <zchil19@freeuni.edu.ge>
2022-01-16 14:25:20 +0400

    changed os path

--- app/infra/connection/database.py
+++ app/infra/connection/database.py
@@ -2,6 +2,8 @@ import os.path
 import sqlite3
 from sqlite3 import Connection
 
+import definitions
+
 
 class Database:
 
@@ -9,7 +11,7 @@ class Database:
 
     def __init__(self, database_name: str):
         self.connection = sqlite3.connect(
-            os.path.dirname(os.path.abspath(__file__)) + "\\database\\" + database_name,
+            definitions.ROOT_DIR + "\\database\\" + database_name,
             check_same_thread=False,
         )
 
---
+++ definitions.py
@@ -0,0 +1,3 @@
+import os.path
+
+ROOT_DIR: str = os.path.dirname(os.path.abspath(__file__))
