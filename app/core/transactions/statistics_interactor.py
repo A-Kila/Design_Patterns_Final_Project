371723659533dbac5ed9c5a814570c78c4cb30bb
Gnems19 <71854341+Gnems19@users.noreply.github.com>
2022-01-16 14:27:31 +0400

    const_in_class

--- app/core/transactions/statistics_interactor.py
+++ app/core/transactions/statistics_interactor.py
@@ -17,9 +17,10 @@ class StatisticsGetResponse:
 @dataclass
 class StatisticsInteractor:
     transaction_repo: ITransactionRepository
+    ADMIN_API_KEY: str = "admin123"
 
     def get_statistics(self, request: StatisticsGetRequest) -> StatisticsGetResponse:
-        if request.admin_api_key != "admin":
+        if request.admin_api_key != self.ADMIN_API_KEY:
             raise Exception("you are not the admin")
         stats: Statistics = self.transaction_repo.get_statistics()
         return StatisticsGetResponse(number_of_transactions=stats.total_transactions,
